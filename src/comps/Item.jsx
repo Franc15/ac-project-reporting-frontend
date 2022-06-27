06d1e5df44f3fe8a5f8bb4b10b8265c095f9db9e
Franc <francissteven1@gmail.com>
2022-06-27 10:54:08 +0300

    make it better

--- src/comps/Item.jsx
+++ src/comps/Item.jsx
@@ -3,10 +3,14 @@ export default function Item(props) {
     <div className="p-2 lg:w-1/5 md:w-1/4 w-full">
       <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
         <div className="flex-grow">
-          <p className="text-gray-900">{props.item.id}</p>
-          <button className="flex w-full mx-auto mt-2 text-white bg-red-400 border-0 py-2 px-2 focus:outline-none hover:bg-red-500 rounded text-md font-bold">
-            <p className="text-center">Done</p>
-          </button>
+          <label class="block text-gray-800 font-bold">
+            <input
+              class="mr-2 leading-tight"
+              value={props.item.id}
+              type="checkbox"
+            />
+            <span class="text-sm">{`${props.item.brand_name} - ${props.item.type_name} - ${props.item.btu}`}</span>
+          </label>
         </div>
       </div>
     </div>
