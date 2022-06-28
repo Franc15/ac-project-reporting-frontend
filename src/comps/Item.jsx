546303919c65ae9b913856f2a40e692539fb906d
Franc <francissteven1@gmail.com>
2022-06-28 11:08:25 +0300

    fix disabled error on checkbox

--- src/comps/Item.jsx
+++ src/comps/Item.jsx
@@ -5,6 +5,8 @@ import { useEffect } from "react";
 
 export default function Item(props) {
   const [isDisabled, setIsDisabled] = useState();
+  const [isDone, setIsDone] = useState();
+
   const getDetails = () => {
     Axios.get(`${API_URL}/record/check/${props.item.id}`, {
       headers: {
@@ -17,6 +19,8 @@ export default function Item(props) {
           console.log("It's a new record");
           setIsDisabled(false);
         } else {
+          // checkbox.checked = true;
+          setIsDone(true);
           setIsDisabled(true);
           console.log("Records added already");
         }
@@ -32,9 +36,7 @@ export default function Item(props) {
   }, []);
 
   const handleClick = () => {
-    // get the checkbox value
     const checkbox = document.getElementById("checkbox" + props.item.id);
-
     // check if checkbox is checked
     if (checkbox.checked) {
       Axios.post(
@@ -59,7 +61,26 @@ export default function Item(props) {
     }
   };
 
-  return (
+  return isDone ? (
+    <div className="p-2 lg:w-1/5 md:w-1/4 w-full">
+      <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+        <div className="flex-grow bg-red-300">
+          <label class="block text-gray-800 font-bold">
+            <input
+              class="mr-2 leading-tight"
+              id={"checkbox" + props.item.id}
+              value={props.item.id}
+              type="checkbox"
+              onChange={handleClick}
+              disabled={isDisabled}
+            />
+            <span class="text-sm">{`${props.item.brand_name} - ${props.item.type_name} - ${props.item.btu}`}</span>
+          </label>
+        </div>
+        <p id={"feed" + props.item.id}></p>
+      </div>
+    </div>
+  ) : (
     <div className="p-2 lg:w-1/5 md:w-1/4 w-full">
       <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
         <div className="flex-grow">
@@ -71,7 +92,6 @@ export default function Item(props) {
               type="checkbox"
               onChange={handleClick}
               disabled={isDisabled}
-              checked={isDisabled}
             />
             <span class="text-sm">{`${props.item.brand_name} - ${props.item.type_name} - ${props.item.btu}`}</span>
           </label>
